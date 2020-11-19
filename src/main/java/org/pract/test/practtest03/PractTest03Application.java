2c5515a6d96c32ded75f36e00268d64eb69f074b
glenn opdycke-hansen <glennoph@gmail.com>
2020-11-18 18:39:36 -0600

    init

---
+++ src/main/java/org/pract/test/practtest03/PractTest03Application.java
@@ -0,0 +1,13 @@
+package org.pract.test.practtest03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractTest03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractTest03Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/pract/test/practtest03/PractTest03ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pract.test.practtest03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractTest03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
