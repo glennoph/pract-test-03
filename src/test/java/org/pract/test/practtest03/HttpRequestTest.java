3922fcfa0ae6adbe96451a8ff31ea628e6188dc1
glenn opdycke-hansen <glennoph@gmail.com>
2020-11-18 19:06:15 -0600

    update dependency in pom.xml, add tests

---
+++ src/test/java/org/pract/test/practtest03/HttpRequestTest.java
@@ -0,0 +1,25 @@
+package org.pract.test.practtest03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingDftMsg() {
+        assertThat(this.restTemplate.getForObject("http://localhost:"
+                        +port+"/",
+                String.class)).contains("hi from spring boot");
+    }
+}
